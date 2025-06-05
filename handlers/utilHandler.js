cbad3e426af998da7faa916989d8c54cff40238f
Mridul-VK <mridulappu@gmail.com>
2025-06-05 23:40:30 +0530

    Fix utilHandler.js

--- handlers/utilHandler.js
+++ handlers/utilHandler.js
@@ -1,13 +1,15 @@
 const getFiles = require("../utils/getFiles");
 const fileNames = getFiles("../utils");
 
-const utilHandler = () => {
+const utilHandler = async () => {
     let utils = {}
     for (let fileName of fileNames) {
-        let utilFile = require((fileName));
+        let utilFile = await require((fileName));
         utils[fileName.split("\\").pop().split(".")[0]] = utilFile;
     }
     return utils;
 }
-const utils = utilHandler()
-module.exports = utils;
+utilHandler().then((result) => {
+    utils = result;
+    module.exports = utils;
+});
