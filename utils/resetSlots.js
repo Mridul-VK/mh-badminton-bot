db3735519f04fc1a72b5f9e8a398d73115c2ac28
Mridul-VK <mridulappu@gmail.com>
2025-06-10 09:01:22 +0530

    Fix timezone issues

--- utils/resetSlots.js
+++ utils/resetSlots.js
@@ -9,7 +9,7 @@ module.exports = resetSlots = async (today) => {
     let currentDatetime = today ? today : new Date();
     await db.query(`INSERT INTO bot_variable (key, value) VALUES ('currentDatetime', $1) ON CONFLICT (key) DO UPDATE SET value = EXCLUDED.value`, [currentDatetime.getTime()]);
     await db.query("DELETE FROM booking");
-    currentDatetime.setUTCHours(15, 30, 0, 0);
+    currentDatetime.setUTCHours(10, 0, 0, 0);
     for (let i = 0; i < 7; i++) {
       await db.query("INSERT INTO booking (user_id, name, slot) VALUES ($1, $2, $3)", ["", "", currentDatetime.getTime() + (i * 60 * 60 * 1000)]);
     }
