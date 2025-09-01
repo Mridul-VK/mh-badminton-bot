e469ea610a0edf3361ced339cf5d322a2fafeb41
Mridul-VK <mridulappu@gmail.com>
2025-09-01 07:23:22 +0530

    minor UI update

--- commands/admin/listAdmins.js
+++ commands/admin/listAdmins.js
@@ -20,7 +20,7 @@ module.exports = {
       await ctx.reply(
         `🚨 *CURRENT ADMINS* 🚨\n${"-".repeat(29)}\n${adminNames.join(
           "\n"
-        )}\n\nUse /add_admin to add an admin or /remove_admin to remove one.`,
+        )}\n\nUse /add\_admin to add an admin or /remove\_admin to remove one.`,
         { parse_mode: "Markdown" }
       );
     } catch (error) {
